ab36995474cd070896dc301cb5830bebd8ad94d8
dekzyd <adekoyadayo2010@gmail.com>
2023-07-14 19:55:13 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import upload from "./upload.png";
+import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+import Navbar from "./components/Navbar";
+
+class App extends Component {
+  state = {
+    selectedFile: null,
+    fileUploadedSuccessfully: false,
+  };
+  onFileChange = (event) => {
+    this.setState({ selectedFile: event.target.files[0] });
+  };
+  onFileUpload = () => {
+    const formData = new FormData();
+    formData.append(
+      "demo file",
+      this.state.selectedFile,
+      this.state.selectedFile.name
+    );
+    //call api
+    console.log(formData);
+    this.setState({ selectedFile: null });
+    this.setState({ fileUploadedSuccessfully: true });
+  };
+
+  fileData = () => {
+    if (this.state.selectedFile) {
+      return (
+        <div>
+          <h2>File Details:</h2>
+          <p>File Name: {this.state.selectedFile.name}</p>
+          <p>File Type: {this.state.selectedFile.type}</p>
+          <p>
+            Last Modified:{" "}
+            {this.state.selectedFile.lastModifiedDate.toDateString()}
+          </p>
+        </div>
+      );
+    } else if (this.state.fileUploadedSuccessfully) {
+      return (
+        <div>
+          <br />
+          <h4>Your file has been uploaded successfully</h4>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <br />
+          <h4>Choose a file and then press the upload button</h4>
+        </div>
+      );
+    }
+  };
+  render() {
+    const { signOut, user } = this.props;
+    console.log(user);
+    return (
+      <>
+        <Navbar user={user} signOut={signOut} />
+        <View className="App">
+          <Card>
+            <Heading level={1}>My Dropbox</Heading>
+            <Image src={upload} className="App-logo" alt="upload" />
+            <div>
+              <input type="file" onChange={this.onFileChange} />
+              <button onClick={this.onFileUpload}>Upload</button>
+            </div>
+            {this.fileData()}
+          </Card>
+          <Button onClick={signOut}>Sign Out</Button>
+        </View>
+      </>
+    );
+  }
+}
+
+export default withAuthenticator(App);
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import "./Navbar.css";
+import { Button } from "@aws-amplify/ui-react";
+const Navbar = ({ signOut, user }) => {
+  const { preferred_username: username } = user.attributes;
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <h1 href="#">My Dropbox</h1>
+        </div>
+
+        <div className="div-2">
+          <span>Hello, {username}</span>
+          <Button onClick={signOut}>Sign Out</Button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
