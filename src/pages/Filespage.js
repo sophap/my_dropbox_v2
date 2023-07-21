4bf589fb526cd4a846f6c6ac21d21a408ee00201
sammyremi <adebayorsamuelas@gmail.com>
2023-07-21 15:12:19 +0100

    made changes

--- src/pages/Filespage.js
+++ src/pages/Filespage.js
@@ -1,8 +1,109 @@
-import "./Filespage.css";
+import React, { useEffect, useState } from 'react';
+// import { Auth } from 'aws-amplify';
+// import "./Filespage.css";
 import file2 from "../assets/file3.txt";
 import file4 from "../assets/file4.jpg";
 
-const Filespage = () => {
+// const Filespage = () => {
+//   const [files, setFiles] = useState([]);
+//   const [accessToken, setAccessToken] = useState('');
+//   const [accessTokenReady, setAccessTokenReady] = useState(false);
+//   useEffect(() => {
+
+//     const getCurrentSession = async () => {
+//       try {
+//         const session = await Auth.currentSession({ bypassCache: true }); // Refresh the token
+//         const accessToken = session.getAccessToken().getJwtToken();
+//         setAccessToken(accessToken);
+//         setAccessTokenReady(true);
+
+//       } catch (error) {
+//         console.log('Error retrieving session:', error);
+//         setAccessTokenReady(false); // Set the token readiness to false in case of an error
+//       }
+//     };
+//     getCurrentSession();
+//   }, []);
+//   useEffect(() => {
+//     if (accessTokenReady) {
+//       fetchFiles();
+//     }
+//   }, [accessTokenReady]);
+
+//     // Function to fetch the list of files from your Lambda function
+//     const fetchFiles = async () => {
+//       try {
+//         const response = await fetch(
+//           `https://oibedj0s0d.execute-api.us-east-1.amazonaws.com/Development/file?token=${encodeURIComponent(
+//             accessToken
+//           )}`,
+//           {
+//           method: 'GET',
+//           headers: {
+//             'Content-Type': 'application/json',
+//           },
+//         }
+//         ); // Replace with your actual Lambda endpoint URL
+//         if (response.ok) {
+//           const data = await response.json();
+//           setFiles(data); // Assuming your Lambda response returns an array of files
+//         } else {
+//           console.log('Failed to fetch files');
+//         }
+//       } catch (error) {
+//         console.error('Error fetching files:', error);
+//       }
+//     };
+//   const handleDownload = (file) => {
+//     // Create a temporary anchor element to trigger the download
+//     const link = document.createElement('a');
+//     link.href = file.fileUrl; // Replace with the actual URL of the file in S3
+//     link.download = file.fileName; // Specify the filename for the download
+//     link.target = '_blank'; // Open the link in a new tab
+//     link.click(); // Programmatically trigger the download
+//   };
+
+//   const handleDelete = async (fileId) => {
+//     const username = (await Auth.currentAuthenticatedUser()).username;
+//     const folderName = username; // Assuming the folder name is the same as the username
+//     const fileName = fileId;
+//     try {
+//       const response = await fetch('https://oibedj0s0d.execute-api.us-east-1.amazonaws.com/Development/file', {
+//         method: 'DELETE',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ folderName, fileName }), // Send the file ID or any other identifier to identify the file to delete
+//       });
+//       if (response.ok) {
+//         console.log('File deleted successfully');
+//         // Update the files state to remove the deleted file from the UI
+//         setFiles((prevFiles) => prevFiles.filter((file) => file.id !== fileId));
+//       } else {
+//         console.log('Failed to delete file');
+//       }
+//     } catch (error) {
+//       console.error('Error deleting file:', error);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h2>View Files</h2>
+//       <ul>
+//         {files.map((file, index) => (
+//           <li key={index}>
+//             {/* Display file details or provide links to download/view files */}
+//             <span>{file.fileName}</span>
+//             <button onClick={() => handleDownload(file)}>Download</button>
+//             <button onClick={() => handleDelete(file.id)}>Delete</button>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
   const getExtenstion = (file) => {
     const split__array = file.split(".");
     const extension = split__array[split__array.length - 1];
@@ -76,6 +177,5 @@ const Filespage = () => {
       </div>
     </div>
   );
-};
 
 export default Filespage;
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,66 +1,107 @@
-import { useState } from "react";
+import React, { useEffect, useState } from 'react';
+import { Auth } from 'aws-amplify';
 import upload from "../upload.png";
 import "../App.css";
 import { Heading, Image, View, Card } from "@aws-amplify/ui-react";
 
-const HomePage = () => {
+const HomePage = ({token, reload}) => {
+  const [accessToken, setAccessToken] = useState('');
+  const [accessTokenReady, setAccessTokenReady] = useState(false);
   const [selectedFile, setSelectedFile] = useState(null);
-  const [fileUploadedSuccessfully, setFileUploadedSuccessfully] =
-    useState(false);
+  const [payload, setPayload] = useState({}); // Use state to hold the payload
 
+  useEffect(() => {
+    // Function to get the current session
+    const getCurrentSession = async () => {
+      try {
+        const session = await Auth.currentSession({ bypassCache: true }); // Refresh the token
+        const accessToken = session.getAccessToken().getJwtToken();
+        setAccessToken(accessToken);
+        setAccessTokenReady(true);
+
+      } catch (error) {
+        console.log('Error retrieving session:', error);
+        setAccessTokenReady(false); // Set the token readiness to false in case of an error
+      }
+    };
+    getCurrentSession();
+  }, []);
   const onFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
-  };
+    event.preventDefault();
+    const file = event.target.files[0];
+    setSelectedFile(file);
 
-  const onFileUpload = () => {
-    const formData = new FormData();
-    formData.append("demo file", selectedFile, selectedFile.name);
+    const reader = new FileReader();
+   
+    reader.onloadend = function() {
+      const base64String = btoa(
+        new Uint8Array(reader.result).reduce((data, byte) => data + String.fromCharCode(byte), '')
+      );
+      const newPayload = {
+        fileData: base64String,
+        fileName: file.name,
+        fileType: file.type,
+        token: accessToken,
+      };
+      setPayload(newPayload);
 
-    //call api
-    // console.log(formData);
-    setSelectedFile(null);
-    setFileUploadedSuccessfully(true);
-  };
-
-  const fileData = () => {
-    if (selectedFile) {
-      return (
-        <div>
-          <h2>File Details:</h2>
-          <p>File Name: {selectedFile.name}</p>
-          <p>File Type: {selectedFile.type}</p>
-          <p>Last Modified: {selectedFile.lastModifiedDate.toDateString()}</p>
-        </div>
-      );
-    } else if (fileUploadedSuccessfully) {
-      return (
-        <div>
-          <br />
-          <h4>Your file has been uploaded successfully</h4>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <br />
-          <h4>Choose a file and then press the upload button</h4>
-        </div>
-      );
     }
+
+    reader.readAsArrayBuffer(file);
   };
-  return (
-    <div className="App">
-      <div className="header">
-        <Heading level={1}>My Dropbox</Heading>
-        <Image src={upload} className="App-logo" alt="upload" />
-        <div className="input__field" style={{ marginTop: "1rem" }}>
-          <input type="file" onChange={onFileChange} />
-          <button onClick={onFileUpload}>Upload</button>
+  
+  async function onFileUpload() {
+    
+    try {
+      if (!accessTokenReady) {
+        console.log('Access token not ready or expired. Please wait or refresh the page.');
+        return;
+      }
+      const response = await fetch(
+        'https://oibedj0s0d.execute-api.us-east-1.amazonaws.com/Development/file',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json', // Set the content type to JSON
+          },
+          body: JSON.stringify(payload), // Convert the fileData to JSON
+        }
+      );
+
+      if (response.ok) {
+        console.log('File uploaded');
+        window.location.reload();
+        setSelectedFile(null);
+      } else {
+        console.log('Uploading failed');
+        console.log(payload);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+      return (
+        <div className="App">
+          <div className="header">
+            <Heading level={1}>My Dropbox</Heading>
+            <Image src={upload} className="App-logo" alt="upload" />
+            <div className="input__field" style={{ marginTop: "1rem" }}>
+              <input type="file" onChange={onFileChange} />
+              <button onClick={onFileUpload} disabled={!accessTokenReady}>Upload</button>
+            </div>
+            {selectedFile && (
+          <div>
+            <br />
+            <h4>File Details:</h4>
+            <p>File Name: {selectedFile.name}</p>
+            <p>File Type: {selectedFile.type}</p>
+            <p>Last Modified: {selectedFile.lastModifiedDate.toDateString()}</p>
+          </div>
+        )}
+          </div>
         </div>
-        {fileData()}
-      </div>
-    </div>
-  );
-};
+      );
+    };
 
 export default HomePage;
+
